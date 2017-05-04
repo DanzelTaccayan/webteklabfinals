616cf22563cded03b7db79f63bfe0d12125a869e
Marvie Yuki <marvieyuki06@gmail.com>
2017-05-04 21:12:43 +0800

    updated feedback_log
    added queries for viewing the name of the sender and the recipient

--- PHPadmin/yomamasbestie/admin/feedback_log.php
+++ PHPadmin/yomamasbestie/admin/feedback_log.php
@@ -1,10 +1,17 @@
- <?php
+<?php
 include '../shared/connection.php';
 include '../shared/auth.php';
 if (isset($_POST['searchUser'])) {
 	$searchUser = $_POST['searchUser'];
 }
 
+$sender="create or replace view recipient as
+SELECT recepient, CONCAT(firstname, middlename, lastname) AS recName from feedback NATURAL JOIN user_details where idUser= recepient";
+$butu="create or replace view sender as SELECT sender, CONCAT(firstname, middlename, lastname) AS senderName from feedback NATURAL JOIN user_details where idUser=sender";
+
+$senderQ = mysqli_query($conn, $sender) ;
+$recepientQ = mysqli_query($conn, $butu);
+			
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,7 +19,6 @@ if (isset($_POST['searchUser'])) {
 	<title>Feedbacks</title>
 </head>
 <body>
-<a href="../home.php">Home</a>
 <h1> Feedbacks </h1>
 <form method="POST">
 	<input type="text" name="searchUser" placeholder="ID/Username/Name/Status/company">
@@ -27,21 +33,22 @@ if (isset($_POST['searchUser'])) {
 <?php
 	
 	if (isset($_POST['searchButton'])) {
-		$feedbacks = "select * from feedback";
+		$feedbacks = "Select recName, senderName, content from feedback JOIN recipient JOIN Sender";
 		$feedbacksQ = mysqli_query($conn, $feedbacks) or die(mysqli_error($conn));
 		while($row = mysqli_fetch_array($feedbacksQ)){
-			echo "<tr><td>" . $row['sender'] . "</td>";
-			echo "<td>" . $row['recepient'] . "</td>";
+			echo "<tr><td>" . $row['senderName'] . "</td>";
+			echo "<td>" . $row['recName'] . "</td>";
 			echo "<td>" . $row['content'] . "</td>";
 		}
 		echo "</tr>";
 
 	}else{
-		$feedbacks = "select * from feedback";
+		$feedbacks = "Select recName, senderName, content from feedback inner join recipient  on  feedback.recepient=recipient.recepient inner JOIN Sender on feedback.sender=sender.sender";
 		$feedbacksQ = mysqli_query($conn, $feedbacks) or die(mysqli_error($conn));
 		while($row = mysqli_fetch_array($feedbacksQ)){
-			echo "<tr><td>" . $row['sender'] . "</td>";
-			echo "<td>" . $row['recepient'] . "</td>";
+						var_dump($row);
+			echo "<tr><td>" . $row['senderName'] . "</td>";
+			echo "<td>" . $row['recName'] . "</td>";
 			echo "<td>" . $row['content'] . "</td>";
 		}
 		echo "</tr>";
@@ -50,6 +57,6 @@ if (isset($_POST['searchUser'])) {
 
 	?>
 </table>
-
+<a href="../home.php">Home</a>
 </body>
 </html>
