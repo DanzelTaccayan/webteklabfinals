0c6917fafa13bf30bfcbbce00b7c45c5c6644668
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2017-05-14 03:58:07 +0800

    added pagination in user.php and manage_users.php

--- PHPadmin/yomamasbestie/admin/users.php
+++ PHPadmin/yomamasbestie/admin/users.php
@@ -20,7 +20,23 @@ if (isset($_POST['searchUser'])) {
 </form>
 
 <?php
-	
+
+$limit = 5;
+$current_page = 1;
+if (isset($_GET['page']) && $_GET['page'] > 0) 
+{
+    $current_page = $_GET['page'];
+}
+$offset = ($current_page * $limit) - $limit;
+
+$user_qry = "select idUsers, username, status, UserType, concat(firstname, ' ', middlename, ' ', lastname) as name, address, email, contactnumber, company from users inner join user_details on idUser = idUsers where usertype!='admin' and status='active';";
+$paginator = "select idUsers, username, status, UserType, concat(firstname, ' ', middlename, ' ', lastname) as name, address, email, contactnumber, company from users inner join user_details on idUser = idUsers where usertype!='admin' and status='active' LIMIT $offset, $limit;";
+$paginatorQ = mysqli_query($conn, $paginator) or die(mysqli_error($conn));
+$userResultQ = mysqli_query($conn, $user_qry) or die(mysqli_error($conn));	
+
+$totalrequest = mysqli_num_rows($userResultQ);
+$pages = ceil($totalrequest/$limit);
+
 	if (isset($_POST['searchButton'])) {
 		$user_qry = "select idUsers, username, status, UserType, concat(firstname, ' ', middlename, ' ', lastname) as name, address, email, contactnumber, company from users inner join user_details on idUser = idUsers where usertype!='admin' and status='active' and (username like '%".$searchUser."%' or concat(firstname, ' ', middlename, ' ', lastname) like '%".$searchUser."%' or company like '%".$searchUser."%');";
 		$user_result = mysqli_query($conn, $user_qry) or die(mysqli_error($conn));
@@ -47,10 +63,8 @@ if (isset($_POST['searchUser'])) {
 		
 
 	}else{
-		$user_qry = "select idUsers, username, status, UserType, concat(firstname, ' ', middlename, ' ', lastname) as name, address, email, contactnumber, company from users inner join user_details on idUser = idUsers where usertype!='admin' and status='active';";
-		$user_result = mysqli_query($conn, $user_qry) or die(mysqli_error($conn));
 
-		if(mysqli_num_rows($user_result) != 0){
+		if(mysqli_num_rows($paginatorQ) != 0){
 			echo "<table border='1'>";
 			echo "<tr>";
 			echo "<th> Username </th>";
@@ -59,7 +73,7 @@ if (isset($_POST['searchUser'])) {
 			echo "<th> Company </th>";
 			echo "<th> Action </th>";
 			echo "</tr>";
-			while($user_arr = mysqli_fetch_array($user_result)){
+			while($user_arr = mysqli_fetch_array($paginatorQ)){
 			echo "<tr><td>" . $user_arr['username'] . "</td>";
 			if($user_arr['UserType'] == 'SP'){
 				echo "<td> Service Provider </td>";
@@ -78,6 +92,28 @@ if (isset($_POST['searchUser'])) {
 	}	
 
 	?>
+		<ul class="pagination">
+			 <!--  <li><a href="#">1</a></li>
+			  <li class="active"><a href="#">2</a></li>
+			  <li><a href="#">3</a></li>
+			  <li><a href="#">4</a></li>
+			  <li><a href="#">5</a></li> -->
+				<?php
+					if($current_page == 1){
+						echo "<li class='disabled'><a href='javascipt:void(0)'>&laquo;</li>";
+					}else{
+						echo "<li><a href='users.php?page=" .($current_page - 1). "'>&laquo;</a></li>";
+					}
+					for($var = 1; $var <= $pages; $var++){
+						echo "<li><a href='users.php?page=" .$var. "'>" .$var."</a></li>";
+					}
+					if($current_page == $pages){
+						echo "<li class='disabled'><a href='javascipt:void(0)'>&raquo;</a></li>";
+					}else{
+						echo "<li><a href='users.php?page=" .($current_page + 1). "'>&raquo;</a></li>";
+					}
+				?>
+			</ul>
 
 
 </table>
