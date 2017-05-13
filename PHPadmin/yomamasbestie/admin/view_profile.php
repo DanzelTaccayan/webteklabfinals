7b5092063253f834547065031e149a095d6819f0
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2017-05-13 21:21:33 +0800

    added request by user in view profile

--- PHPadmin/yomamasbestie/admin/view_profile.php
+++ PHPadmin/yomamasbestie/admin/view_profile.php
@@ -23,7 +23,8 @@ if ($userDetailsQ) {
 	$status = $row['status'];
 
 }
-
+	
+	echo "<h1>User Details</h1>";
 	echo "User ID: $usersId<br>";
 	echo "First Name: $firstName<br>";
 	echo "Address: $address <br>";
@@ -31,7 +32,48 @@ if ($userDetailsQ) {
 	echo "Contact Number: $contactNumber<br>";
 	echo "Company: $company<br>";
 	echo "Username: $userName<br>";
-	echo "Status: $status";
+	echo "Status: $status <br><br>";
+	echo "<h2>Request</h2>";
+?>
+<html>
+<head></head>
+<body>
+<table border="1">
+<tr>
+	<th> Status </th>
+	<th> Requested By </th>
+	<th> Requested To </th>
+	<th> Service Name </th>
+	<th> Request Date </th>
+	<th> Updated At </th>
 
+</tr>
+
+<?php
+	$query = "Select * from request where requested_by='$idUsers'";
+	$serviceQuery = "Select service_name from request natural join services order by request_date";
+	$reqByQuery = "Select CONCAT(firstName,' ',middleName,' ',lastName) as reqBy from user_details join request where idUser = requested_by ORDER BY request_date";
+	$reqToQuery = "Select CONCAT(firstName,' ',middleName,' ',lastName) as reqTo from user_details join request where idUser = requested_to ORDER BY request_date";
+
+	$resultQ = mysqli_query($conn, $query) or die(mysqli_error($conn));
+	$resultServiceQ = mysqli_query($conn, $serviceQuery) or die(mysqli_error($conn));
+	$resultReqByQ = mysqli_query($conn, $reqByQuery) or die(mysqli_error($conn));
+	$resultReqToQ = mysqli_query($conn, $reqToQuery) or die(mysqli_error($conn));
+
+	while($row = mysqli_fetch_array($resultQ)){
+			$serviceRow = mysqli_fetch_array($resultServiceQ);
+			$reqByRow = mysqli_fetch_array($resultReqByQ);
+			$reqToRow = mysqli_fetch_array($resultReqToQ);
+
+		echo "<tr><td>" . $row['status'] . "</td>";
+				echo "<td>" . $reqByRow['reqBy'] . "</td>";
+				echo "<td>" . $reqToRow['reqTo'] . "</td>";
+				echo "<td>" . $serviceRow['service_name'] . "</td>";
+				echo "<td>" . $row['request_date'] . "</td>";
+				echo "<td>" . $row['updated_at'] . "</td>";
+	}
 
 ?>
+</table>
+</body>
+</html>
