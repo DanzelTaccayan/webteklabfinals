8295720bdc1f21959dbaaca4390e79308d0866b8
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2017-05-14 13:47:30 +0800

    added pagination in transaction.php

--- PHPadmin/yomamasbestie/admin/transactions.php
+++ PHPadmin/yomamasbestie/admin/transactions.php
@@ -25,6 +25,50 @@ $transactions_result = mysqli_query($conn, $transactions) or die(mysqli_error($c
 </form>
 
 <?php
+
+		$limit = 1;
+		$current_page = 1;
+		if (isset($_GET['page']) && $_GET['page'] > 0) 
+		{
+		    $current_page = $_GET['page'];
+		}
+		$offset = ($current_page * $limit) - $limit;
+
+
+		$viewCust = "CREATE OR REPLACE VIEW customerName AS
+        SELECT 
+		idUser, transaction_status as stat, transaction_id as a,
+        concat(firstName, ' ', middleName, ' ', lastName) as custName 
+   		FROM
+        transaction
+            inner JOIN
+        user_details on idUser=cust_id where UserType = 'customer';";
+        
+        $viewSp = "CREATE OR REPLACE VIEW serpName AS
+		SELECT 
+		idUser, transaction_status, transaction_id as b,
+        concat(firstName, ' ', middleName, ' ', lastName) as spName 
+	    FROM
+	        transaction
+	            inner JOIN
+	        user_details on idUser=sp_id
+	    WHERE
+        UserType = 'SP';";
+
+        $allTran = "SELECT 
+				    stat, custName, spName ,a
+					FROM
+				    customerName
+				        inner JOIN
+				    serpName on a=b LIMIT $offset, $limit;";
+
+		$allTranQ = mysqli_query($conn, $allTran); 
+        $viewCustQ = mysqli_query($conn, $viewCust);
+        $viewSpQ = mysqli_query($conn, $viewSp);
+
+        $totalrequest = mysqli_num_rows($transactions_result);
+        $pages = ceil($totalrequest/$limit);
+
 		if(isset($_POST['searchTrans'])){
 			if($_POST['statusTransac'] == 'all'){
 				if(mysqli_num_rows($transactions_result) != 0){
@@ -35,34 +79,26 @@ $transactions_result = mysqli_query($conn, $transactions) or die(mysqli_error($c
 					echo  "<th> Status </th>";
 					echo  "<th> Action </th>";
 					echo  "</tr>";
-				while($transaction_arr = mysqli_fetch_array($transactions_result)){
-				//sp
-				$sp_num = $transaction_arr['sp_id'];
-				$nameSp = "SELECT concat(firstName, ' ', middleName, ' ', lastName) as name from user_details where idUser = '$sp_num'";
-				$nameSp_result = mysqli_query($conn, $nameSp) or die(mysqli_error($conn));
-				$sp_arr = mysqli_fetch_array($nameSp_result);
-				//customer
-				$cust_num = $transaction_arr['cust_id'];
-				$nameCust = "SELECT concat(firstName, ' ', middleName, ' ', lastName) as name from user_details where idUser = '$cust_num'";
-				$nameCust_result = mysqli_query($conn, $nameCust) or die(mysqli_error($conn));
-				$cust_arr = mysqli_fetch_array($nameCust_result);
-
-
-				echo "<tr><td>" . $sp_arr['name'] . "</td>";
-				echo "<td>" . $cust_arr['name'] . "</td>";
-				echo "<td>" . $transaction_arr['transaction_status'] . "</td>";
-				echo "<td><a href='view_transaction.php?transaction_id=". $transaction_arr['transaction_id'] . "'>View Details </a></tr>";
+				while($transaction_arr = mysqli_fetch_array($allTranQ)){
+					echo "<tr><td>" . $transaction_arr['custName'] . "</td>";
+					echo "<td>" . $transaction_arr['spName'] . "</td>";
+					echo "<td>" . $transaction_arr['stat'] . "</td>";
+					echo "<td><a href='view_transaction.php?transaction_id=". $transaction_arr['a'] . "'>View Details </a></tr>";
 				}
 				}else{
 					echo "<h1> No transactions </h1>";
 				}
-			}else if(
-				$_POST['statusTransac'] == 'on-going'){
+			}else if($_POST['statusTransac'] == 'on-going'){
 						
 					
-					$newtransac = "SELECT * from transaction where transaction_status = 'ongoing' order by transaction_id desc;";
-					$newtransac_result = mysqli_query($conn, $newtransac);
-					if(mysqli_num_rows($newtransac_result) != 0){
+					$allTran = "SELECT 
+				    stat, custName, spName ,a
+					FROM
+				    customerName
+				        inner JOIN
+				    serpName on a=b where stat='ongoing';";
+					$allTranQ = mysqli_query($conn, $allTran);   
+					if(mysqli_num_rows($transactions_result) != 0){
 						echo "<table border='1'>";
 						echo  "<tr>";
 						echo  "<th> Service Provider </th>";
@@ -70,31 +106,26 @@ $transactions_result = mysqli_query($conn, $transactions) or die(mysqli_error($c
 						echo  "<th> Status </th>";
 						echo  "<th> Action </th>";
 						echo  "</tr>";
-					while($newtransac_array = mysqli_fetch_array($newtransac_result)){
-						//sp
-						$sp_num = $newtransac_array['sp_id'];
-						$nameSp = "SELECT concat(firstName, ' ', middleName, ' ', lastName) as name from user_details where idUser = '$sp_num'";
-						$nameSp_result = mysqli_query($conn, $nameSp) or die(mysqli_error($conn));
-						$sp_arr = mysqli_fetch_array($nameSp_result);
-						//customer
-						$cust_num = $newtransac_array['cust_id'];
-						$nameCust = "SELECT concat(firstName, ' ', middleName, ' ', lastName) as name from user_details where idUser = '$cust_num'";
-						$nameCust_result = mysqli_query($conn, $nameCust) or die(mysqli_error($conn));
-						$cust_arr = mysqli_fetch_array($nameCust_result);
-
-						
-
-
-						echo "<tr><td>" . $sp_arr['name'] . "</td>";
-						echo "<td>" . $cust_arr['name'] . "</td>";
-						echo "<td>" . $newtransac_array['transaction_status'] . "</td>";
-						echo "<td><a href='view_transaction.php?transaction_id=". $newtransac_array['transaction_id'] . "'>View Details </a></tr>";
+					while($transaction_arr = mysqli_fetch_array($allTranQ)){
+						echo "<tr><td>" . $transaction_arr['custName'] . "</td>";
+						echo "<td>" . $transaction_arr['spName'] . "</td>";
+						echo "<td>" . $transaction_arr['stat'] . "</td>";
+						echo "<td><a href='view_transaction.php?transaction_id=". $transaction_arr['a'] . "'>View Details </a></tr>";
 					}
 				}else{
 				echo "<h1> No on-going transactions </h1>";
 				}
+
 			}else if($_POST['statusTransac'] == 'done'){
-				if(mysqli_num_rows($transactions_result) != 0){
+				
+					$allTran = "SELECT 
+				    stat , custName, spName ,a
+					FROM
+				    customerName
+				        inner JOIN
+				    serpName on a=b where stat='done';";
+					$allTranQ = mysqli_query($conn, $allTran);   
+					if(mysqli_num_rows($transactions_result) != 0){
 						echo "<table border='1'>";
 						echo  "<tr>";
 						echo  "<th> Service Provider </th>";
@@ -102,31 +133,16 @@ $transactions_result = mysqli_query($conn, $transactions) or die(mysqli_error($c
 						echo  "<th> Status </th>";
 						echo  "<th> Action </th>";
 						echo  "</tr>";
-					$newtransac_done = "SELECT * from transaction where transaction_status = 'done' order by transaction_id desc;";
-					$newtransac_done_result = mysqli_query($conn, $newtransac_done);
-					while($new_transac_done_array = mysqli_fetch_array($newtransac_done_result)){
-						//sp
-						$sp_num = $new_transac_done_array['sp_id'];
-						$nameSp = "SELECT concat(firstName, ' ', middleName, ' ', lastName) as name from user_details where idUser = '$sp_num'";
-						$nameSp_result = mysqli_query($conn, $nameSp) or die(mysqli_error($conn));
-						$sp_arr = mysqli_fetch_array($nameSp_result);
-						//customer
-						$cust_num = $new_transac_done_array['cust_id'];
-						$nameCust = "SELECT concat(firstName, ' ', middleName, ' ', lastName) as name from user_details where idUser = '$cust_num'";
-						$nameCust_result = mysqli_query($conn, $nameCust) or die(mysqli_error($conn));
-						$cust_arr = mysqli_fetch_array($nameCust_result);
-
-						
-
-						echo "<tr><td>" . $sp_arr['name'] . "</td>";
-						echo "<td>" . $cust_arr['name'] . "</td>";
-						echo "<td>" . $new_transac_done_array['transaction_status'] . "</td>";
-						echo "<td><a href='view_transaction.php?transaction_id=". $new_transac_done_array['transaction_id'] . "'>View Details </a></tr>";
+					while($transaction_arr = mysqli_fetch_array($allTranQ)){
+						echo "<tr><td>" . $transaction_arr['custName'] . "</td>";
+						echo "<td>" . $transaction_arr['spName'] . "</td>";
+						echo "<td>" . $transaction_arr['stat'] . "</td>";
+						echo "<td><a href='view_transaction.php?transaction_id=". $transaction_arr['a'] . "'>View Details </a></tr>";
 					}
 				}else{
-				echo "<h1> No transactions completed </h1>";
+					echo "<h1> No transactions </h1>";
 				}
-			}
+			}	
 		}else{
 			if(mysqli_num_rows($transactions_result) != 0){
 					echo "<table border='1'>";
@@ -136,27 +152,14 @@ $transactions_result = mysqli_query($conn, $transactions) or die(mysqli_error($c
 					echo  "<th> Status </th>";
 					echo  "<th> Action </th>";
 					echo  "</tr>";
-				while($transaction_arr = mysqli_fetch_array($transactions_result)){
-				//sp
-					$sp_num = $transaction_arr['sp_id'];
-					$nameSp = "SELECT concat(firstName, ' ', middleName, ' ', lastName) as name from user_details where idUser = '$sp_num'";
-					$nameSp_result = mysqli_query($conn, $nameSp) or die(mysqli_error($conn));
-					$sp_arr = mysqli_fetch_array($nameSp_result);
-					//customer
-					$cust_num = $transaction_arr['cust_id'];
-					$nameCust = "SELECT concat(firstName, ' ', middleName, ' ', lastName) as name from user_details where idUser = '$cust_num'";
-					$nameCust_result = mysqli_query($conn, $nameCust) or die(mysqli_error($conn));
-					$cust_arr = mysqli_fetch_array($nameCust_result);
+				while($transaction_arr = mysqli_fetch_array($allTranQ)){
 
-					
+					echo "<tr><td>" . $transaction_arr['spName'] . "</td>";
+					echo "<td>" . $transaction_arr['custName'] . "</td>";
+					echo "<td>" . $transaction_arr['stat'] . "</td>";
+					echo "<td><a href='view_transaction.php?transaction_id=". $transaction_arr['a'] . "'>View Details </a></tr>";
 
-
-					echo "<tr><td>" . $sp_arr['name'] . "</td>";
-					echo "<td>" . $cust_arr['name'] . "</td>";
-					echo "<td>" . $transaction_arr['transaction_status'] . "</td>";
-					echo "<td><a href='view_transaction.php?transaction_id=". $transaction_arr['transaction_id'] . "'>View Details </a></tr>";
-
-			}
+				}
 
 			}else{
 				echo "<h1> No transactions </h1>";
@@ -167,6 +170,29 @@ $transactions_result = mysqli_query($conn, $transactions) or die(mysqli_error($c
 
 		
 	?>
+	<ul>
+	 <!--  <li><a href="#">1</a></li>
+	  <li class="active"><a href="#">2</a></li>
+	  <li><a href="#">3</a></li>
+	  <li><a href="#">4</a></li>
+	  <li><a href="#">5</a></li> -->
+	  	<?php
+			if($current_page == 1){
+				echo "<li class='disabled'><a href='javascipt:void(0)'>&laquo;</a></li>";
+			}else{
+				echo "<li><a href='transactions.php?page=" .($current_page - 1). "'>&laquo;</a></li>";
+			}
+			for($var = 1; $var <= $pages; $var++){
+				echo "<li><a href='transactions.php?page=" .$var. "'>" .$var."</a></li>";
+			}
+			if($current_page == $pages){
+				echo "<li class='disabled'><a href='javascipt:void(0)'>&raquo;</a></li>";
+			}else{
+				$a = $current_page + 1;
+				echo "<li><a href='transactions.php?page=" .($current_page + 1). "'>&raquo;</a></li>";
+			}
+		?>
+	</ul>
 
 </table>
 
